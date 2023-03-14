a93ac2ca7e2c9821be5b0d7237cbc5d3b4d76df9
bap <60724808+ba-p@users.noreply.github.com>
2023-03-14 21:26:44 +0700

    Add api for handling login logic (#4)
    * FE: Reclean logo and button color
    
    * feat: Add api for handling login form
    
    * format: Clean code

---
+++ app/enums/enums.ts
@@ -0,0 +1,9 @@
+export enum ResponseStatusCode {
+    OK = 200,
+    CREATED = 201,
+    FAILED = 400,
+    UNAUTHORIZED = 401,
+    FORBIDDEN = 403,
+    NOT_FOUND = 404,
+    INTERNAL_SERVER_ERROR = 500,
+}
